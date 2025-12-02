1ecb9bd6d9d93520f2c5c4819159899605505513
Andrei Purcaru <andrei.purcaru@teampicnic.com>
2025-12-02 22:49:53 +0100

    Implement part 1 and prototype part 2

--- src/test/kotlin/days/Day1Test.kt
+++ src/test/kotlin/days/Day1Test.kt
@@ -5,10 +5,10 @@ import kotlin.test.Test
 class Day1Test {
     @Test
     fun solve() {
-        val day1Example1 = Day1("Day1Example.txt")
+        val day1Example = Day1("Day1Example.txt")
         val day1 = Day1("Day1.txt")
 
-        println(day1Example1.solve())
+        println(day1Example.solve())
         println(day1.solve())
     }
 }
---
+++ src/test/kotlin/days/Day2Test.kt
@@ -0,0 +1,19 @@
+package days
+
+import kotlin.test.Test
+
+class Day2Test {
+    @Test
+    fun solve() {
+        val day2Example = Day2("Day2Example.txt")
+//        println(day2Example.solvePart1())
+        println(day2Example.solvePart2())
+
+        val day2 = Day2("Day2.txt")
+//        println(day2.solvePart1())
+//        val day1 = Day1("Day1.txt")
+//
+//        println(day1Example1.solve())
+//        println(day1.solve())
+    }
+}
