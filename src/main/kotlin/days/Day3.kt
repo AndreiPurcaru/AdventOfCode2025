1d2fffedf3e2c9db22f948e8138de574da4257d0
Andrei Purcaru <andrei.purcaru@teampicnic.com>
2025-12-03 23:08:30 +0100

    Migrate to only use the part 2 impl

--- src/main/kotlin/days/Day3.kt
+++ src/main/kotlin/days/Day3.kt
@@ -1,23 +1,22 @@
 package days
 
-import java.lang.Math.pow
 import kotlin.math.pow
 import kotlin.text.indexOf
 
 class Day3(override val fileName: String) : Day {
-    private val ITERATIONS = 12
-
     override fun solvePart1(): String {
-
-        return extractLines().sumOf { findMaxJoltage(it.trim()) }.toString()
+        return extractLines().sumOf { findMaxJoltageRec(it.trim(), 2) }.toString()
     }
 
     override fun solvePart2(): String {
-
-        val list = extractLines().map { findMaxJoltageRec(it.trim()) }.toString()
-        return extractLines().sumOf { findMaxJoltageRec(it.trim()) }.toString()
+        return extractLines().sumOf { findMaxJoltageRec(it.trim(), 12) }.toString()
     }
 
+    fun extractLines(): List<String> {
+        return readFile().lines().filter { it.isNotBlank() }
+    }
+
+    @Deprecated("Use findMaxJoltageRec instead, much nicer")
     private fun findMaxJoltage(batteryBank: String): Int {
         // Leave at least one battery so we can have a range
         val leftMax = batteryBank.dropLast(1).max()
@@ -28,15 +27,12 @@ class Day3(override val fileName: String) : Day {
         return leftMax.digitToInt() * 10 + rightMax
     }
 
-    fun extractLines(): List<String> {
-        return readFile().lines().filter { it.isNotBlank() }
-    }
+    private fun findMaxJoltageRec(batteryBank: String, iterations: Int): Long {
+        if (iterations == 0) return 0L
 
-    private fun findMaxJoltageRec(batteryBank: String, foundMaxes: Int = 0): Long {
-        if (foundMaxes == ITERATIONS) return 0L
+        val max = batteryBank.dropLast(iterations - 1).max()
 
-        val max = batteryBank.dropLast(ITERATIONS - foundMaxes - 1).max()
-
-        return max.digitToInt() * 10.0.pow((ITERATIONS - foundMaxes - 1).toDouble()).toLong() + findMaxJoltageRec(batteryBank.substring(batteryBank.indexOf(max) + 1), foundMaxes + 1)
+        return max.digitToInt() * 10.0.pow((iterations - 1).toDouble())
+            .toLong() + findMaxJoltageRec(batteryBank.substring(batteryBank.indexOf(max) + 1), iterations - 1)
     }
 }
