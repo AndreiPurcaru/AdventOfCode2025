bbe050a1be2bd69298684a44034fd9c30258c25a
Andrei Purcaru <andrei.purcaru@teampicnic.com>
2025-12-05 21:19:01 +0100

    Implement functionally and with no vars

--- src/main/kotlin/days/Day5.kt
+++ src/main/kotlin/days/Day5.kt
@@ -16,38 +16,29 @@ class Day5(override val fileName: String) : Day {
     override fun solvePart2(): String {
         val input = readFile()
 
-        var ranges = input.substringBefore("\n\n").lines().map {
+        val ranges = input.substringBefore("\n\n").lines().map {
             it.substringBefore("-").toLong()..it.substringAfter("-").toLong()
         }
 
-        var currentSize = ranges.size
-        var newSize = 0
+        return mergeAllRanges(ranges).sumOf { it.last - it.first + 1 }.toString()
+    }
 
-        while (currentSize != newSize) {
-            val sortedRanges = ranges.sortedBy { it.first }
+    private fun mergeAllRanges(ranges: List<LongRange>): List<LongRange> {
+        return generateSequence(ranges to 0) { (currentRanges, previousSize) ->
+            if (previousSize == currentRanges.size) return@generateSequence null
 
-            if (currentSize % 2 == 1) {
+            val sortedRanges = currentRanges.sortedBy { it.first }
+
+            val merged = if (previousSize % 2 == 1) {
                 val rangesTryLeft = sortedRanges.chunked(2).flatMap { attemptMerge(it) }
-                val rangesTryRight =  listOf(sortedRanges[0]) + sortedRanges.drop(1).chunked(2).flatMap { attemptMerge(it) }
-                if (rangesTryLeft.size < rangesTryRight.size) {
-                    ranges = rangesTryLeft
-                    currentSize = newSize
-                    newSize = ranges.size
-                } else {
-                    ranges = rangesTryRight
-                    currentSize = newSize
-                    newSize = ranges.size
-                }
-                continue
+                val rangesTryRight = listOf(sortedRanges[0]) + sortedRanges.drop(1).chunked(2).flatMap { attemptMerge(it) }
+                if (rangesTryLeft.size < rangesTryRight.size) rangesTryLeft else rangesTryRight
+            } else {
+                sortedRanges.chunked(2).flatMap { attemptMerge(it) }
             }
 
-            ranges = sortedRanges.chunked(2).flatMap { attemptMerge(it) }
-            currentSize = newSize
-            newSize = ranges.size
-        }
-
-        return ranges.sumOf { it.last - it.first + 1 }.toString()
-
+            merged to sortedRanges.size
+        }.last().first
     }
 
     private fun attemptMerge(maybeTuple: List<LongRange>): List<LongRange> {
