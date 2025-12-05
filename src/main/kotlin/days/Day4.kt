6531308504b390e171189077dad5a58c55cf72f0
Andrei Purcaru <andrei.purcaru@teampicnic.com>
2025-12-05 02:06:46 +0100

    Implement Day 1 checkpoint

---
+++ src/main/kotlin/days/Day4.kt
@@ -0,0 +1,49 @@
+package days
+
+class Day4(override val fileName: String) : Day {
+    override fun solvePart1(): String {
+        val matrix = extractMatrix()
+
+        return matrix.indices.sumOf { i ->
+            matrix[i].indices.filter { checkRemovable(matrix, i, it) }.size
+        }.toString()
+    }
+
+
+    override fun solvePart2(): String {
+        TODO("Not yet implemented")
+    }
+
+    private fun extractMatrix(): Array<Array<Char>> =
+        readFile().lines().map { line -> line.map { it }.toTypedArray() }.toTypedArray()
+
+    private fun checkRemovable(matrix: Array<Array<Char>>, i: Int, j: Int): Boolean {
+        return if (matrix[i][j] == '@') {
+            findSurroundingRolls(matrix, i, j) < 4
+        } else {
+            false
+        }
+    }
+
+    private fun findSurroundingRolls(matrix: Array<Array<Char>>, i: Int, j: Int): Int {
+        return Direction.entries.sumOf { (x, y) ->
+            if (i + x !in matrix.indices || j + y !in matrix[i].indices) 0
+            else if (matrix[i + x][j + y] == '.') 0
+            else 1
+        }
+    }
+
+    private enum class Direction(val x: Int, val y: Int) {
+        UP(0, -1),
+        DOWN(0, 1),
+        LEFT(-1, 0),
+        RIGHT(1, 0),
+        UP_LEFT(-1, -1),
+        UP_RIGHT(1, -1),
+        DOWN_LEFT(-1, 1),
+        DOWN_RIGHT(1, 1);
+
+        operator fun component1() = x
+        operator fun component2() = y
+    }
+}
---
+++ src/test/kotlin/days/Day4Test.kt
@@ -0,0 +1,14 @@
+package days
+
+import kotlin.test.Test
+
+class Day4Test {
+    @Test
+    fun solve() {
+        val day4Example = Day4("Day4Example.txt")
+        val day4 = Day4("Day4.txt")
+
+//        println(day4Example.solvePart1())
+        println(day4.solvePart1())
+    }
+}
