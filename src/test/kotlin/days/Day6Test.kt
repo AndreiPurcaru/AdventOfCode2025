d50f7b1f48b997ed2f94161862bba0e4f6fdfa48
Andrei Purcaru <andreipurcaru@yahoo.com>
2025-12-07 01:28:29 +0100

    Implement Day 6, both parts (#7)

---
+++ src/test/kotlin/days/Day6Test.kt
@@ -0,0 +1,14 @@
+package days
+
+import kotlin.test.Test
+
+class Day6Test {
+    @Test
+    fun solve() {
+        val day6Example = Day6("Day6Example.txt")
+        val day6 = Day6("Day6.txt")
+
+//        println(day6Example.solvePart2())
+        println(day6.solvePart2())
+    }
+}
